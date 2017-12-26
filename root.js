33503ee4b3084b5d532cf132b4230ce3230c5f11
uzulove <uyio2002@gmail.com>
2017-12-26 16:28:35 +0900

    Revert "Modify README.md"
    This reverts commit 3c726a0b7adba9333a5a37d2f11abb247a390439.

--- root.js
+++ root.js
@@ -1,7 +1,5 @@
 // author: uzulove
 
-const Animal = require('./models/animal')
-
 // root router's handlers
 
 function notLogined(req, res) {
@@ -20,25 +18,6 @@ function goToMainPage(req, res) {
 }
 
 function test(res, res) {
-  var animal = new Animal()
-  animal.name = 'A'
-  animal.age = 35
-
-  var child1 = {}
-  child1.name = 'B'
-  child1.age = 5
-  var child2 = {}
-  child2.name = 'C'
-  child2.age = 2
-
-  var children = [child1, child2]
-
-  animal.children = children
-
-  animal.save()
-  .then((res)=>console.log(res))
-  .catch((err)=>console.log(err))
-
   res.loadHtml('test.html')
 }
 
